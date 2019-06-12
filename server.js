9ac7ec81f86f593475fae6b5f20fd8e59212edd4
Renato Reis <email@renatoamreis.com>
2019-06-12 15:59:47 +0200

    Adding

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const cors = require("cors")
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    connectionString: 'process.env.DATABASE_URL',
-    ssl: true,
+    connectionString : 'process.env.DATABASE_URL',
+    ssl: true
   }
 });
 
