e99afcc83f180708c3694342674abf26fb5f89e0
Renato Reis <email@renatoamreis.com>
2019-06-12 15:43:43 +0200

    Adding DB configuration

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const cors = require("cors")
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'cmMadrid',
-    database : 'smart-brain'
+    connectionString: 'process.env.DATABASE_URL',
+    ssl: true,
   }
 });
 
